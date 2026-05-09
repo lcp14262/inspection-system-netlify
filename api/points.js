4324451df8bce02bdb5f4f01a20a5c206c232f28
lcp14262 <108786008+lcp14262@users.noreply.github.com>
2026-05-09 10:22:30 +0800

    Update points.js

--- api/points.js
+++ api/points.js
@@ -1,5 +1,5 @@
 const CHECKIN_POINTS = {
-   'A001': { name: '1号厂房东侧', area: '生产车间', lat: 31.226186, lng: 118.259766, radius: 100 },
+'A001': { name: '1号厂房东侧', area: '生产车间', lat: 31.230834, lng: 118.173690, radius: 100 },
     'A002': { name: '安庆工厂', area: '仓储区', lat: 30.5215, lng: 117.0478, radius: 200 },
     'B001': { name: '合肥工厂', area: '危化品区', lat: 31.7608, lng: 117.2027, radius: 200 },
 };
